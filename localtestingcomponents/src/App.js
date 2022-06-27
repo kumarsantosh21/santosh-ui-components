470a0a26bc8f4cf0765671ea2daf1f0de116e6bd
santosh <santoorvlss4321@gmail.com>
2022-06-27 17:17:34 +0530

    some files  for testinglocal component files

---
+++ localtestingcomponents/src/App.js
@@ -0,0 +1,25 @@
+import { CheckingmuiButton, Button, Alert } from "santosh-ui-components";
+
+function App() {
+  return (
+    <div>
+      <CheckingmuiButton child="Button" />
+      <Button>test</Button>
+      <Alert
+        variant="outlined"
+        severity="success"
+        alertTitle="This is title"
+        // hasAction
+        // actionText="UNDO"
+        // actionButtonFunciton={() => {
+        //   console.log("should trigger hasAction Button");
+        // }}
+        onClose={() => {}}
+      >
+        This is alert
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
